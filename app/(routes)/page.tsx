2f2bfbf4bcf57f3fd21b4cf3625a52cad4fe6e32
Md Rakibul Islam <rakib02.08.23@gmail.com>
2023-11-04 20:16:19 +0600

    first commit

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,18 @@
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("f77439c3-0ab2-4d9b-88e0-8dcc1a1b575e");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
