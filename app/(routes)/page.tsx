975114148161313325683756efcb907dc79c72cc
Md Rakibul Islam <rakib02.08.23@gmail.com>
2023-11-05 14:01:54 +0600

    added and updated

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,15 +1,22 @@
 import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
 import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
   const billboard = await getBillboard("f77439c3-0ab2-4d9b-88e0-8dcc1a1b575e");
+  const products = await getProducts({isFeatured: true});
+
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
       </div>
     </Container>
   );
